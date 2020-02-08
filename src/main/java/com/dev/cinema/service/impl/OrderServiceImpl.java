95b44c1c5be9deb7abe67e8ed1c9bec7b3e7a1ae
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2020-02-08 13:45:26 +0200

    adde Order(and all layers)

---
+++ src/main/java/com/dev/cinema/service/impl/OrderServiceImpl.java
@@ -0,0 +1,36 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.dao.OrderDao;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
+import com.dev.cinema.model.Order;
+import com.dev.cinema.model.ShoppingCart;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.OrderService;
+import com.dev.cinema.service.ShoppingCartService;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private static OrderDao orderDao;
+    @Inject
+    private static ShoppingCartService shoppingCartService;
+
+    @Override
+    public Order completeOrder(User user) {
+        ShoppingCart shoppingCart = shoppingCartService.getByUser(user);
+        Order order = new Order();
+        order.setTickets(shoppingCart.getTickets());
+        order.setUser(user);
+        order.setOrderDate(LocalDate.now());
+        shoppingCartService.clear(shoppingCart);
+        return orderDao.add(order);
+    }
+
+    @Override
+    public List<Order> getOrderHistory(User user) {
+        return orderDao.getUserOrders(user);
+    }
+}
